f2fa65a5b4ac607765c23e4b1a91b2eae7de0222
samern92 <32775563+samern92@users.noreply.github.com>
2021-03-19 21:49:07 -0400

    Add files via upload

---
+++ PredCell_model.py
@@ -0,0 +1,81 @@
+### IN PROGRESS
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+class StateUnit(nn.Module):
+    def __init__(self, layer_level, timestep, initializer,isTopLayer = False):
+        super().__init__()
+        self.layer_level = layer_level
+        self.timestep = timestep
+        self.state_ = st_initializer.sample() # the idea is to sample from a distribution
+        self.recon_ = 0 # reconstructions at all other time points will be determined by the state
+        self.V = np.random.uniform(-1,1,size = (3,3)) # figure out size?
+    def forward(self, BU_err, TD_err):
+        self.timestep += 1
+        if isTopLayer:
+            self.state_ = nn.LSTM(self.state_, BU_err)
+        else:
+            self.state_ = nn.LSTM(self.state_, BU_err, TD_err)
+        self.recon_ = self.V * self.state_
+
+        
+class ErrorUnit(nn.Module):
+    def __init__(self, layer_level, timestep, err_initializer, W_initializer):
+        super().__init__()
+        self.layer_level = layer_level
+        self.timestep = timestep
+        self.TD_err = err_initializer.sample()
+        self.BU_err = 0
+        self.W = W_initializer.sample() 
+    def forward(self, state_, recon_):
+        self.timestep += 1
+        self.TD_err = np.abs(state_ - recon_)
+        self.BU_err = self.W * self.TD_err
+    def get_timestep():
+        return self.timestep
+        
+class PredCells(nn.Module):
+    def __init__(self, num_layers, total_timesteps):
+        self.num_layers = num_layers
+        self.total_timesteps = total_timesteps
+##        self.st_units = []
+##        self.err_units = []
+        self.state_initval = 0
+        self.error_initval = 0
+        for lyr in range(num_layers):
+            self.st_units.append(StateUnit(lyr,0,initializer))
+            self.err_units.append(ErrorUnit(lyr,0,initializer))
+##    def initial_pass(self.st_units, self.err_units):
+##        # get initial reconstructions
+##        for lyr in range(num_layers,0,-1):
+##            if lyr == num_layers:
+##                self.st_units[lyr].recon_ = self.st_units[lyr].forward(self.err_units[lyr].BU_err,0)
+##            elif lyr > 0:
+##                self.st_units[lyr].recon_ = self.st_units[lyr].forward(self.err_units[lyr].BU_err,self.err_units[lyr+1].TD_err)
+##            else:
+##                pass # lowest layer does not generate a reconstruction
+            
+    def forward(self, input_sentence):
+        for t in range(self.total_timesteps):
+            # input_char at each t is a one-hot character encoding
+            input_char = input_sentence[t]
+            for lyr in range(num_layers):
+                if lyr == 0:
+                    # override the value 
+                    self.st_units[lyr] = input_char
+                else:
+                    self.st_units[lyr] = self.st_units[lyr].forward(self.err_units[lyr-1].BU_err, self.err_units[lyr].TD_err)
+                if lyr < num_layers:
+                    self.err_units[lyr].forward(self.st_units[lyr].state_, self.st_units[lyr+1].recon_)
+                else:
+                    self.err_units[lyr].forward()
+                
+          
+
+
+
+
+            
+            
